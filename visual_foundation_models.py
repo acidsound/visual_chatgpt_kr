1dc205a0a5f7a8a5e6979a29f34e4f0fd57d4e6e
v-sheyin <v-sheyin@microsoft.com>
2023-03-15 14:10:20 +0800

    update text2image

--- visual_foundation_models.py
+++ visual_foundation_models.py
@@ -137,26 +137,27 @@ class InstructPix2Pix:
 
 class Text2Image:
     def __init__(self, device):
-        print("Initializing Text2Image to %s" % device)
+        print(f"Initializing Text2Image to {device}")
         self.device = device
-        self.pipe = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5",torch_dtype=torch.float16)
-        self.text_refine_tokenizer = AutoTokenizer.from_pretrained("Gustavosta/MagicPrompt-Stable-Diffusion")
-        self.text_refine_model = AutoModelForCausalLM.from_pretrained("Gustavosta/MagicPrompt-Stable-Diffusion")
-        self.text_refine_gpt2_pipe = pipeline("text-generation", model=self.text_refine_model,
-                                              tokenizer=self.text_refine_tokenizer, device=self.device)
+        self.torch_dtype = torch.float16 if 'cuda' in device else torch.float32
+        self.pipe = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5",
+                                                            torch_dtype=self.torch_dtype)
         self.pipe.to(device)
+        self.a_prompt = 'best quality, extremely detailed'
+        self.n_prompt = 'longbody, lowres, bad anatomy, bad hands, missing fingers, extra digit, ' \
+                        'fewer digits, cropped, worst quality, low quality'
 
     @prompts(name="Generate Image From User Input Text",
              description="useful when you want to generate an image from a user input text and save it to a file. "
                          "like: generate an image of an object or something, or generate an image that includes some objects. "
                          "The input to this tool should be a string, representing the text used to generate image. ")
     def inference(self, text):
-        image_filename = os.path.join('image', str(uuid.uuid4())[0:8] + ".png")
-        refined_text = self.text_refine_gpt2_pipe(text)[0]["generated_text"]
-        image = self.pipe(refined_text).images[0]
+        image_filename = os.path.join('image', f"{str(uuid.uuid4())[:8]}.png")
+        prompt = text + ', ' + self.a_prompt
+        image = self.pipe(prompt, negative_prompt=self.n_prompt).images[0]
         image.save(image_filename)
         print(
-            f"\nProcessed Text2Image, Input Text: {text}, Refined Text: {refined_text}, Output Image: {image_filename}")
+            f"\nProcessed Text2Image, Input Text: {text}, Output Image: {image_filename}")
         return image_filename
 
 
